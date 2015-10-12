994d0c9a4409cc880840e1ba196f90e729c4d2a8
Kevin Zhang <klz1@rodney.personalrobotics.ri.cmu.edu>
2015-10-12 15:21:28 -0400

    Save point just before fixing.

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Porting EyeKin over to Linux.
 
 Current work in progress.
+
+Install OpenCV 2.4.11
+https://gist.github.com/dynamicguy/3d1fce8dae65e765f7c4
--- eyeKinMain.cpp
+++ eyeKinMain.cpp
@@ -30,7 +30,7 @@
 
 #include <libfreenect2/libfreenect2.hpp>
 #include <libfreenect2/frame_listener_impl.h>
-#include <libfreenect2/threading.h>
+#include <libfreenect2/logger.h>
 #include <libfreenect2/registration.h>
 #include <libfreenect2/packet_pipeline.h>
 #ifdef EXAMPLES_WITH_OPENGL_SUPPORT
