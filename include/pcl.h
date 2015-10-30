e45fb9f5198b3691e2aa538e9213b1594eff28ae
Kevin Zhang <klz1@rodney.personalrobotics.ri.cmu.edu>
2015-10-30 13:42:20 -0400

    Finally Compiles

--- include/pcl.h
+++ include/pcl.h
@@ -1,6 +1,8 @@
 #ifndef __PCL_H__
 #define __PCL_H__
 
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
 #include <pcl/common/common_headers.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/statistical_outlier_removal.h>
